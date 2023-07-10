d1799a816b516a77b13e0f20b167fcb46186b800
yashp119 <125194772+yashp119@users.noreply.github.com>
2023-07-10 18:49:18 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import { Router, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+import ProjectsPage from "./pages/Projects";
+import BookPage from "./pages/Bookshelf";
+import { Global } from "./styles/Global";
+
+
+function App() {
+  return (
+    <Router>
+      <Global />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/projects" component={ProjectsPage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/bookshelf" component={BookPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
