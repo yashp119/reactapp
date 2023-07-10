c73f8deaa6e7388fadabe65740033c24f1e2d367
yashp119 <125194772+yashp119@users.noreply.github.com>
2023-07-10 18:47:45 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,34 @@
+import {Header} from "../components/Header";
+import { Descriptors } from "../components/HomeMain";
+import { formatOf } from "../components/extras/customFormatters";
+import {PageSupportingContent} from "../components/PageSupportingContent";
+import HomePageContent from "./contents/HomePageContents"
+import Footer from "../components/Footer";
+import Container from "../styles/Global";
+import Divider from "../components/extras/Divider";
+
+const HomePage = () => {
+  return (
+      <Container>
+          <Header />
+          <Descriptors
+            plaintext="I AM A..."
+            config={{
+              strings: [
+                `${formatOf("ENGINEER")}`,
+                `${formatOf("STUDENT")}`,
+                `${formatOf("MUSICIAN")}`,
+                `${formatOf("WRITER")}`,
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+          <Divider />
+          <PageSupportingContent content={HomePageContent} />
+      </Container>
+      <Footer />
+  );
+};
+
+export default HomePage;
